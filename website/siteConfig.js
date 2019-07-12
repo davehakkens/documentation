2f87839a088433198814ace6d03edebbf8169d89
Dave Hakkens <dave@davehakkens.nl>
2019-07-12 12:34:13 +0200

    added build

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Precious Plastic Toolbox', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://davehakkens.github.io/documentation/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://davehakkens.github.io', // Your website URL
+  baseUrl: '/documentation', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Toolbox',
-  organizationName: 'Precious Plastic',
+  projectName: 'documentation',
+  organizationName: 'davehakkens',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
